089568e99ea9232c7f3de17f59ff90526ae72b78
andrerco001 <andreoliveira.adv@hotmail.com>
2021-12-04 08:56:06 -0500

    Project created

---
+++ src/main/java/com/springboot/systemspringboot/SystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.systemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/systemspringboot/SystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.systemspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
